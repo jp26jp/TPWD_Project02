e46001767e07f1dabd24984f0b2258ea977810e1
jp26jp <rUNIT026>
2018-11-15 06:21:03 -0700

    Slight code reformatting

--- league_builder.py
+++ league_builder.py
@@ -5,7 +5,6 @@ from player import Player
 allPlayers = []
 file = open("teams.txt", "w+")
 
-
 def load_players():
     with open("./soccer_players.csv", newline="") as csvFile:
         reader = csv.DictReader(csvFile, delimiter=",")
@@ -13,7 +12,6 @@ def load_players():
     for player in players:
         allPlayers.append(Player(player["Name"], player["Height (inches)"], player["Soccer Experience"], player["Guardian Name(s)"]))
     allPlayers.sort()
-
 
 def assign_players_team(team1, team2, team3):
     i = 0
@@ -28,7 +26,6 @@ def write_team_info_to_file(name, team):
     file.write(name + "\n")
     for member in team:
         file.write("{}, {}, {}\n".format(member.name, member.experience, member.guardians))
-
 
 if __name__ == '__main__':
     dragons = []
