85ee8b54959ade236cebd2ec0bedfad5be191b9c
jp26jp <rUNIT026>
2018-12-25 03:45:23 -0700

    Added comments and add "exceeded" work

--- league_builder.py
+++ league_builder.py
@@ -2,30 +2,67 @@ import csv
 
 from player import Player
 
-allPlayers = []
-file = open("teams.txt", "w+")
+all_players = []
+teams = open("teams.txt", "w+")
+
 
 def load_players():
+    """
+    Loads players from the .csv file, adds them to the `all_players` array,
+    and sorts them by their experience
+    """
     with open("./soccer_players.csv", newline="") as csvFile:
         reader = csv.DictReader(csvFile, delimiter=",")
         players = list(reader)
     for player in players:
-        allPlayers.append(Player(player["Name"], player["Height (inches)"], player["Soccer Experience"], player["Guardian Name(s)"]))
-    allPlayers.sort()
+        all_players.append(Player(player["Name"], player["Height (inches)"], player["Soccer Experience"], player["Guardian Name(s)"]))
+    all_players.sort()
+
 
 def assign_players_team(team1, team2, team3):
-    i = 0
-    for player in allPlayers:
-        i += 1;
-        teamNumber = i % 3
+    """
+    Loops through all_players array and adds one player at a time to each team.
+    Player's are divided equally by experience because the all_players array
+    is sorted by player's experience.
+
+    :param team1: an empty array of the first team
+    :param team2: an empty array of the second team
+    :param team3: an empty array of the third team
+    """
+    for index, player in enumerate(all_players):
+        teamNumber = index % 3
         if teamNumber == 0: team1.append(player)
         if teamNumber == 1: team2.append(player)
         if teamNumber == 2: team3.append(player)
 
+
 def write_team_info_to_file(name, team):
-    file.write(name + "\n")
+    """
+    Creates a file with each team's roster.
+
+    :param name: the name of the team
+    :param team: an array of the players on the team
+    """
+    teams.write(name + "\n")
     for member in team:
-        file.write("{}, {}, {}\n".format(member.name, member.experience, member.guardians))
+        teams.write("{}, {}, {}\n".format(member.name, member.experience, member.guardians))
+
+
+def write_player_letters(team, team_name):
+    """
+    Creates a unique file for each team members guardians informing them
+    which team their child belongs to and when practice is
+
+    :param team: the array of team members
+    :param team_name: a string that is the name of the team
+    :return:
+    """
+    for member in team:
+        name = member.name.lower().replace(" ", "_")
+        file = open("{}.txt".format(name), "w+")
+        file.write("Dear {},\n".format(member.guardians))
+        file.write("Your human, {}, has been accepted by the terrifying {}. Practice is at sunrise tomorrow.".format(member.name, team_name))
+
 
 if __name__ == '__main__':
     dragons = []
@@ -38,3 +75,7 @@ if __name__ == '__main__':
     write_team_info_to_file("Dragons", dragons)
     write_team_info_to_file("\n\nSharks", sharks)
     write_team_info_to_file("\n\nRaptors", raptors)
+
+    write_player_letters(dragons, "Dragons")
+    write_player_letters(sharks, "Sharks")
+    write_player_letters(raptors, "Raptors")
